bb3bf67044e54412e2307b05e43ebd8901f3cc73
naushadac <naushadac@gmail.com>
2016-11-07 00:19:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,30 @@ app.get('/ui/instruction.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'instruction.html'));
 });
 
+app.get('/ui/aptitude.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'aptitude.png'));
+});
+
+app.get('/ui/back1.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'back1.png'));
+});
+
+app.get('/ui/board.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'board.png'));
+});
+
+app.get('/ui/chemistry.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'chemistry.png'));
+});
+
+app.get('/ui/continue.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'continue.png'));
+});
+
+app.get('/ui/gk.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'gk.png'));
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
